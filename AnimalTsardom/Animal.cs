2cf7b5114653468266c844ba34294a4d3027ac10
Preslava-P <preslavap20@gmail.com>
2023-02-15 10:25:41 +0200

    first commit

---
+++ AnimalTsardom/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalTsardom
+{
+    public abstract class Animal:IMakeNoise, IMakeTrick
+    {
+        protected string name;
+        protected int age;
+
+        public Animal(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public virtual string MakeNoise()
+        {
+            return $"My name is{this.name}. I am {this.age} years old";
+        }
+
+        public virtual string MakeTrick()
+        {
+            return $"Look at my trick!";
+        }
+        
+        
+    }
+}
---
+++ AnimalTsardom/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalTsardom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Dog("Balkan", 5);
+            Console.WriteLine(animal1.MakeNoise());
+            Console.WriteLine(animal1.MakeTrick());
+            Animal animal2 = new Cat("Artyom", 3);
+            Console.WriteLine(animal2.MakeNoise());
+            Console.WriteLine(animal2.MakeTrick());
+
+            
+
+        }
+    }
+}
